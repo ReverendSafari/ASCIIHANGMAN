083fc9c7a6b924fc0932eb6c8244f877d89d2bfd
ReverendSafari <s.swanson24@ncf.edu>
2023-05-02 13:33:21 -0700

    For Lainey

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Hangman!");
     }
+}
+
+class wordBank {
+    ArrayList<String> cars = new ArrayList<String>();
+    ArrayList<String> wordBankCustom  = new ArrayList<String>();
+
+    public static void createWordBank() {
+        String stop = "";
+        /*while(stop == "") {
+
+
+        }*/
+    }
+
 }
