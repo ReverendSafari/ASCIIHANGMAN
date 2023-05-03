a37e37da4e4af9ab37efca0c78d9ae9fdc327e40
ReverendSafari <s.swanson24@ncf.edu>
2023-05-03 06:47:08 -0700

    Getting there
    Getting basic classes setup

--- src/Main.java
+++ src/Main.java
@@ -1,31 +1,91 @@
 import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
 
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello Hangman!");
-    }
-}
-// hello
-class HangMan {
-    String secretWord;
-    int incorrectGuesses;
-    char currentGuess;
-    int maxGuesses;
-    Boolean gameOver;
-}
 class wordBank {
-    ArrayList<String> cars = new ArrayList<String>();
-    ArrayList<String> wordBankCustom  = new ArrayList<String>();
+    static ArrayList<String> wordBankStatic = new ArrayList<String>();
+    static ArrayList<String> wordBankCustom  = new ArrayList<String>();
 
     public static void createWordBank() {
         Scanner s = new Scanner(System.in);
         String currentWord;
-        String stop = "";
-        while(stop == "") {
+        Boolean stop = true;
+        while(stop) {
+            System.out.println("Please enter a word to add to the bank--Or type 1 to stop:");
             currentWord = s.nextLine().trim();
-            wordBankCustom.add(currentWord);
+            if (currentWord.equals("1")) {
+                stop = false;
+                System.out.println("Word Bank is complete!");
+
+            }
+            else {
+                wordBankCustom.add(currentWord);
+            }
         }
     }
+    public static String generateRandomWord(boolean r) {
+        Random selector = new Random();
+        if(r) {
+            int limit = wordBankCustom.size();
+            int randomNum = selector.nextInt(limit);
+            return wordBankCustom.get(randomNum);
+        }
+        else {
+            int limit = wordBankStatic.size();
+            int randomNum = selector.nextInt(limit);
+            return wordBankCustom.get(randomNum);
+        }
+    }
+    public void printBank() {
+        for (int i=0; i<wordBankCustom.size(); i++) {
+            System.out.println(wordBankCustom.get(i));
+        }
+    }
+    public static void main(String[] args) {
+        wordBank r = new wordBank();
+        createWordBank();
+        r.printBank();
     }
 
+}
+
+// hello
+class HangMan {
+    String secretWord;
+    int incorrectGuesses;
+
+    public char getCurrentGuess() {
+        return currentGuess;
+    }
+
+    public void setCurrentGuess(char currentGuess) {
+        this.currentGuess = currentGuess;
+    }
+
+    char currentGuess;
+    int maxGuesses = 6;
+    Boolean gameOver = false;
+
+    void Hangman(boolean c) {
+        if (c) {
+            secretWord = wordBank.generateRandomWord(c);
+        }
+        else {
+            secretWord = wordBank.generateRandomWord(c);
+        }
+
+    }
+    boolean CheckGameOver() {
+        if (incorrectGuesses >= maxGuesses) {
+            gameOver = true;
+            return gameOver;
+        }
+        return gameOver;
+    }
+}
+
+class HangManGame extends HangMan {
+    String[] statusLabel;
+    String playerLabel;
+    String[] boardStates;
 }
